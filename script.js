62b931840a7f25d73fe9de97ac056dca42ba4bd0
anupambera882 <71135339+anupambera882@users.noreply.github.com>
2023-06-01 12:53:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// function myFunction(id) {
+//     setTimeout(() => {
+//         console.log("This is a function");
+//     }, 2000);
+//     let element = document.getElementById(id);
+//     element.classList.add("active");
+// }
